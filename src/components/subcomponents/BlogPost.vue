1d407681491ec63e9d3a3a7245cc8f60eef78607
Yau Chun Yin <yauchunyin0309@gmail.com>
2025-10-28 06:15:21 +0000

    feat: complete ex1 and ex2

--- src/components/subcomponents/BlogPost.vue
+++ src/components/subcomponents/BlogPost.vue
@@ -2,12 +2,30 @@
     export default {
         // Add code if/as necessary
         // Hint: Add properties subject, entry and mood
+        props: {
+            subject: String,
+            entry: String,
+            mood: String
+        },
+
+        computed: {
+            imgLoc() {
+                // Vue understands that /assets is under the public folder
+                return `/assets/${this.mood.toLowerCase()}.png`
+            }
+        }
     }
 </script>
 
 
 <template>
     <!-- TODO: add your template code here. Use boostrap card --> 
-
+    <div class="card" style="width: 18rem;">
+        <img :src="imgLoc" class="card-img-top">
+        <div class="card-body">
+            <h5 class="card-title">{{ subject }}</h5>
+            <p class="card-text">{{ entry }}</p>
+        </div>
+    </div>
 </template>
 
